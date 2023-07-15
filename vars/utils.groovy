2c2861e79da405afcba856fc3d83de5b1d283f8e
Kenny Baltazar alanoca <developer.innovaappstar@gmail.com>
2023-07-15 10:31:18 -0500

    jenkins modified

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,7 +1,11 @@
+import com.shared.library.utils.FileExtractorUtils
+
 String getMessage(){
     return "hola mundo"
 }
 
-String getVersionFromPomFile(String path){
-
+String getVersionFromPackageJSON(String path){
+    def fileContent = readFile(file: path)
+    def versionFromPackageJson = new FileExtractorUtils().readVersionFromPackageJsonFile(fileContent)
+    return versionFromPackageJson
 }
